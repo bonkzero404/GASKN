55964f3290ec3eee021abfad1d7500ac6eaaa382
Janitra Panji <bonkzero404@gmail.com>
2022-12-17 01:52:01 +0700

    Validate request menu

--- features/menu/dto/menu_request.go
+++ features/menu/dto/menu_request.go
@@ -3,9 +3,9 @@ package dto
 import "github.com/bonkzero404/gaskn/database/stores"
 
 type MenuRequest struct {
-	MenuName        string          `json:"menu_name" validate:"required"`
-	MenuDescription string          `json:"menu_description"`
+	MenuName        string          `json:"menu_name" validate:"required,alpha"`
+	MenuDescription string          `json:"menu_description,alphanum"`
 	ParentId        string          `json:"parent_id,omitempty" validate:"omitempty,uuid4"`
-	MenuUrl         string          `json:"menu_url"`
+	MenuUrl         string          `json:"menu_url" validate:"required,uri"`
 	MenuType        stores.MenuType `json:"menu_type" validate:"required,oneof=BO CL"`
 }
