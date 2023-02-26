b7d2fb76057ecba6bb2dd9155f85ff1d1c9af010
Janitra Panji <bonkzero404@gmail.com>
2023-02-26 16:50:03 +0700

    Refactor extra features

--- app/bootstrap.go
+++ app/bootstrap.go
@@ -1,7 +1,7 @@
 package app
 
 import (
-	"github.com/bonkzero404/gaskn/app/middleware"
+	"github.com/bonkzero404/gaskn/app/extra_features"
 	"github.com/bonkzero404/gaskn/config"
 	"github.com/bonkzero404/gaskn/features/auth"
 	cl "github.com/bonkzero404/gaskn/features/client"
@@ -14,65 +14,6 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
-func extrasFeature(app *fiber.App) {
-	var route = utils.GasknRouter{}
-
-	route.Set(app).Group(utils.SetupApiGroup() + "/features").SetGroupName("Features")
-	// Get feature lists
-	route.Get(
-		"/",
-		middleware.Authenticate(),
-		middleware.Permission(),
-		func(c *fiber.Ctx) error {
-			return utils.ApiOk(c, utils.ExtractRouteAsFeatures(c, false))
-		}).
-		SetRouteName("FeatureLists").
-		SetRouteDescriptionKeyLang("route:features").
-		Execute()
-
-	// Get feature per group
-	route.Get(
-		"/group",
-		middleware.Authenticate(),
-		middleware.Permission(),
-		func(c *fiber.Ctx) error {
-			return utils.ApiOk(c, utils.FeaturesGroupLists(c, false))
-		}).
-		SetRouteName("FeatureGroupLists").
-		SetRouteDescriptionKeyLang("route:features:group").
-		Execute()
-
-	if config.Config("TENANCY") == "true" {
-		route.Set(app).Group(utils.SetupSubApiGroup() + "/features").SetGroupName("Client/Features")
-
-		// Get feature lists Tenant
-		route.Get(
-			"/",
-			middleware.Authenticate(),
-			middleware.Permission(),
-			func(c *fiber.Ctx) error {
-				return utils.ApiOk(c, utils.ExtractRouteAsFeatures(c, true))
-			}).
-			SetRouteName("FeatureLists").
-			SetRouteDescriptionKeyLang("route:features").
-			SetRouteTenant(true).
-			Execute()
-
-		// Get feature per group tenant
-		route.Get(
-			"/group",
-			middleware.Authenticate(),
-			middleware.Permission(),
-			func(c *fiber.Ctx) error {
-				return utils.ApiOk(c, utils.FeaturesGroupLists(c, true))
-			}).
-			SetRouteName("FeatureGroupLists").
-			SetRouteDescriptionKeyLang("route:features:group").
-			SetRouteTenant(true).
-			Execute()
-	}
-}
-
 // Bootstrap /*
 func Bootstrap(app *fiber.App) {
 
@@ -80,7 +21,7 @@ func Bootstrap(app *fiber.App) {
 	app.Get("/monitor", monitor.New())
 
 	// Register features
-	extrasFeature(app)
+	extra_features.ExtrasFeature(app)
 
 	// Register feature user
 	user.RegisterFeature(app)
